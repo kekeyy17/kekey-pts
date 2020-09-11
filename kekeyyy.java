bab2646513afc0b692f723058085af6d4cf26f0f
kekeyy17 <71141321+kekeyy17@users.noreply.github.com>
2020-09-11 23:52:53 +0700

    Add files via upload

---
+++ kekeyyy.java
@@ -0,0 +1,41 @@
+package sample;
+
+public class kekeyyy {
+
+	public static void main(String[] args) {
+		
+			        String M ="ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+			        char a = M.charAt(12);
+			        char b = M.charAt(14);
+			        char c = M.charAt(2);
+			        char d = M.charAt(7);
+			        char e = M.charAt(27);
+			        char f = M.charAt(17);
+			        char g = M.charAt(8);
+			        char h = M.charAt(25);
+			        char i = M.charAt(10);
+			        char j = M.charAt(24);
+			        char k = M.charAt(27);
+			        char l = M.charAt(5);
+			        char m = M.charAt(0);
+			        char n = M.charAt(20);
+			        char o = M.charAt(25);
+			        char p = M.charAt(0);
+			        char q = M.charAt(13);
+			        char r = M.charAt(27);
+			        char s = M.charAt(17);
+			        char t = M.charAt(0);
+			        char u = M.charAt(12);
+			        char v = M.charAt(3);
+			        char w = M.charAt(7);
+			        char x = M.charAt(0);
+			        char y = M.charAt(13);
+			        char z = M.charAt(8);
+
+
+			    System.out.println(a+""+b+""+c+""+d+""+e+""+f+""+g+""+h+""+i+""+j+""+k+""+l+""+m+""+n+""+o+""+p+""+q+""+r+""+s+""+t+""+u+""+v+""+w+""+x+""+y+""+z+"");
+
+
+	}
+
+}
